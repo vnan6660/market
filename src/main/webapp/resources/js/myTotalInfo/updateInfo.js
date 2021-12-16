da69997415d14aff8d0c93962166fc3da8dc53fb
김혜경 <kimhye@LAPTOP-SU1E0UPQ>
2021-12-16 19:45:25 +0900

    [혜경]회원수정페이지 본인 이메일은 이메일 중복에서 제외시킴

--- src/main/webapp/resources/js/myTotalInfo/updateInfo.js
+++ src/main/webapp/resources/js/myTotalInfo/updateInfo.js
@@ -97,13 +97,9 @@ $(function() {
 	
 	
 	$('#updateInfoBtn').click(function() {
-//		updateInfo().then(aa);
-		/*if (updateInfo()) {
-			aa();
-		}*/
 		updateInfo().then(function(ajaxData){
 			console.log(ajaxData);
-			alert('3.aa실행');
+			//alert('3.aa실행');
 			if(ajaxData==true){
 				if (confirm('수정하시겠습니까?')) {
 				$.ajax({
@@ -126,228 +122,199 @@ $(function() {
 					alert("취소되었습니다");
 				}
 			}else{
-				alert("alsdkfjlskdfjls");
+				//alert("alsdkfjlskdfjls");
 			}
 			
 		});
 	});
 	
-});
+});//레디펑션 마침
 
 
 
 //2.updateInfo()메서드
 function updateInfo() {
 	return new Promise(function(resolve, reject){
-	alert("1.updateInfo실행");
-	var returnValue = false;
-
-	//유효성검사
-	var pwOnlyEngNumSpecial = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/;
-	var nmOnlyHangulEng = /^[가-힣a-zA-Z]+$/;
-	var emailOnly = /^[a-zA-Z0-9_\.\-]+$/;
-	var phonelOnly = /^(?=.*?[0-9]).{4,4}$/;
-	var pattern = /\s/g;
-	var csPs = $("#csPs").val();
-	var csPsConfirm = $("#csPsConfirm").val();
-	var csNm = $("#csNm").val();
-	var csPhoneOne = $("#csPhoneOne").val();
-	var csPhoneTwo = $("#csPhoneTwo").val();
-	var csPhoneThree = $("#csPhoneThree").val();
-	var csPhone = csPhoneOne + csPhoneTwo + csPhoneThree;
-	var csEmailOne = $("#csEmailOne").val();
-	var csEmailWriteInput = $("#csEmailWriteInput").val()
-	var csEmailTwo = $("#csEmailTwo").val();
-	var csEmail = csEmailOne + '@' + csEmailWriteInput + csEmailTwo;
-	if (csEmailTwo == "직접입력") {
-		csEmail = csEmailOne + '@' + csEmailWriteInput;
-	}
-	//전화번호 속성추가
-	$('input[name=csPhone]').attr('value', csPhone);
-	//이메일 속성추가
-	$('input[name=csEmail]').attr('value', csEmail);
-
-	// ================ 비밀번호 ================ //
-	//1. 빈값 안됨
-	if (csPs == "") {
-		$("#csPwCheck").text("비밀번호를 입력해주세요").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csPwCheck").text("");
-	}
-
-	//2. 정규식 맞는지
-	if (!pwOnlyEngNumSpecial.test(csPs)) {
-		$("#csPwCheck").text("형식에 알맞은 비밀번호를 입력해주세요.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csPwCheck").text("");
-	}
-
-	//3.공백이 들어갔는지
-	if (csPs.match(pattern)) {
-		$("#csPwCheck").text("공백이 존재합니다.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csPwCheck").text("");
-	}
-
-	// ================ 비밀번호 재확인 ================ //
-	//1. 빈값 안됨
-	if (csPsConfirm == "") {
-		$("#csPwConfirm").text("비밀번호를 입력해주세요").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csPwConfirm").text("");
-	}
-
-	//2.공백이 들어갔는지
-	if (csPsConfirm.match(pattern)) {
-		$("#csPwConfirm").text("공백이 존재합니다.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csPwConfirm").text("");
-	}
-
-	//3.비밀번호랑 입력값 같은지
-	if (csPsConfirm != csPs) {
-		$("#csPwConfirm").text("비밀번호를 다시 확인해주세요").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csPwConfirm").text("");
-	}
-
-	// ================ 이름 ================ //
-	//1. 빈값 안됨
-	if (csNm == "") {
-		$("#csNmConfirm").text("이름을 입력해주세요").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csNmConfirm").text("");
-	}
-
-	//2. 정규식 맞는지
-	if (!nmOnlyHangulEng.test(csNm)) {
-		$("#csNmConfirm").text("형식에 알맞은 이름을 입력해주세요.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csNmConfirm").text("");
-	}
-
-	//3.공백이 들어갔는지
-	if (csNm.match(pattern)) {
-		$("#csNmConfirm").text("공백이 존재합니다.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csNmConfirm").text("");
-	}
-
-	// ================ 전화번호 ================ //
-	//1.빈값 안됨
-	if (csPhoneTwo == "" || csPhoneThree == "") {
-		$("#phoneChk").text("전화번호를 입력해주세요").css("color", "red");
-		return returnValue;
-	} else {
-		$("#phoneChk").text("");
-	}
-
-	//2.전화번호 정규식 맞는지
-	if (!phonelOnly.test(csPhoneTwo) || !phonelOnly.test(csPhoneThree)) {
-		$("#phoneChk").text("형식에 알맞은 전화번호를 입력해주세요.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#phoneChk").text("");
-	}
-
-	//3.공백이 들어갔는지
-	if (csPhoneTwo.match(pattern) || csPhoneThree.match(pattern)) {
-		$("#phoneChk").text("공백이 존재합니다.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#phoneChk").text("");
-	}
-
-	// ================ 생년월일 ================ //
-	//회원정보수정 만14세 미만 가입 금지
-	var date = new Date(); //Date생성자 생성
-	var thisY = date.getFullYear(); //현재년도를 YYYY로 반환한걸 thisY변수에 넣는다
-	var selectY = $("#yearBox").val(); //선택한 년도
-	if (thisY - selectY < 16) { // 올해 - 선택한년도가 16미만이면
-		$("#ageConfirm").text("*만 14세 미만 아동의 회원정보수정은 받고 있지 않습니다").css("color", "red");
-		return returnValue;
-	}
-
-	// ================ 이메일 ================ //
-	//1. 빈값 안됨
-	if (csEmailOne == "" || csEmailTwo == "") {
-		$("#csEmailChk").text("이메일을 입력해주세요").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csEmailChk").text("");
-	}
-
-	//2. 이메일 앞자리 정규식 맞는지
-	if (!emailOnly.test(csEmailOne)) {
-		$("#csEmailChk").text("형식에 알맞은 이메일을 입력해주세요.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csEmailChk").text("");
-	}
-
-	//3.공백이 들어갔는지
-	if (csEmailOne.match(pattern) || csEmailTwo.match(pattern)) {
-		$("#csEmailChk").text("공백이 존재합니다.").css("color", "red");
-		return returnValue;
-	} else {
-		$("#csEmailChk").text("");
-	}
-
-	//4.이메일 선택이면 도메인을 선택해주세요 띄우기
-	if (csEmailTwo == '선택') {
-		$("#csEmailChk").text("도메인을 선택해주세요.").css("color", "red");
-		return returnValue;
-	} else {
-		csEmailOne = "";
-	}
-
-	//5.이미 사용중인 이메일인지
-	var resultVal="";
-	var csEmailOne = $("#csEmailOne").val(); //id가 csEmailOne인 값의 내용을 csEmailOne변수에 넣는다.
-	var csEmailTwo = $("#csEmailTwo").val(); //id가 csEmailTwo인 값의 내용을 csEmailTwo변수에 넣는다.
-	var csEmail = csEmailOne + '@' + csEmailTwo;
-	var data = {};//빈 객체 생성
-	data.csEmail = csEmail; //위에서 작성한 변수값을 'data.속성'에 넣는 작업
-	alert('2.emailChk실행');
+		//alert("1.updateInfo실행");
+		var returnValue = false;
 	
-	$.ajax({
-		url: '/myInfo/infoEmailChk',//요청 url
-		type: 'POST',//post타입
-		contentType: 'application/json',//서버에서 어떤 타입(json, html, text...)을 받을 것인지를 의미. json(key:value)형태의 데이터타입을 사용
-		data: JSON.stringify(data),//서버에서 어떤 타입(json, html, text...)을 받을 것인지를 의미. json(key:value)형태의 데이터타입을 사용
-		dataType: 'json', //요청과 함께 보낼 데이터
-		success: function(result) {//성공했을시 수행하는 function
-			if (result == 0) { //DB에 저장된 이메일개수가 0개면
-				$("#csEmailChk").text("");
-				resultVal = true;
-				resolve(resultVal);
-			} else {//DB에 저장된 이메일이 1개있다면(DB에 있단 소리니깐 존재하는 이메일)
-				$("#csEmailChk").text("존재하는 이메일입니다. 다시 입력해주세요.").css("color", "red");
-				resultVal = false;
-				resolve(resultVal);
-			}
-		},
-		error: function() {
-			alert("관리자에게 문의하세요");
+		//유효성검사
+		var pwOnlyEngNumSpecial = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,16}$/;
+		var nmOnlyHangulEng = /^[가-힣a-zA-Z]+$/;
+		var emailOnly = /^[a-zA-Z0-9_\.\-]+$/;
+		var phonelOnly = /^(?=.*?[0-9]).{4,4}$/;
+		var pattern = /\s/g;
+		var csPs = $("#csPs").val();
+		var csPsConfirm = $("#csPsConfirm").val();
+		var csNm = $("#csNm").val();
+		var csPhoneOne = $("#csPhoneOne").val();
+		var csPhoneTwo = $("#csPhoneTwo").val();
+		var csPhoneThree = $("#csPhoneThree").val();
+		var csPhone = csPhoneOne + csPhoneTwo + csPhoneThree;
+		var csEmailOne = $("#csEmailOne").val();
+		var csEmailWriteInput = $("#csEmailWriteInput").val()
+		var csEmailTwo = $("#csEmailTwo").val();
+		var csEmail = csEmailOne + '@' + csEmailWriteInput + csEmailTwo;
+		if (csEmailTwo == "직접입력") {
+			csEmail = csEmailOne + '@' + csEmailWriteInput;
 		}
-	});
-	/*function emailChk(){
-	return new Promise(function(resolve, reject){
+		//전화번호 속성추가
+		$('input[name=csPhone]').attr('value', csPhone);
+		//이메일 속성추가
+		$('input[name=csEmail]').attr('value', csEmail);
+	
+		// ================ 비밀번호 ================ //
+		//1. 빈값 안됨
+		if (csPs == "") {
+			$("#csPwCheck").text("비밀번호를 입력해주세요").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csPwCheck").text("");
+		}
+	
+		//2. 정규식 맞는지
+		if (!pwOnlyEngNumSpecial.test(csPs)) {
+			$("#csPwCheck").text("형식에 알맞은 비밀번호를 입력해주세요.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csPwCheck").text("");
+		}
+	
+		//3.공백이 들어갔는지
+		if (csPs.match(pattern)) {
+			$("#csPwCheck").text("공백이 존재합니다.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csPwCheck").text("");
+		}
+	
+		// ================ 비밀번호 재확인 ================ //
+		//1. 빈값 안됨
+		if (csPsConfirm == "") {
+			$("#csPwConfirm").text("비밀번호를 입력해주세요").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csPwConfirm").text("");
+		}
+	
+		//2.공백이 들어갔는지
+		if (csPsConfirm.match(pattern)) {
+			$("#csPwConfirm").text("공백이 존재합니다.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csPwConfirm").text("");
+		}
+	
+		//3.비밀번호랑 입력값 같은지
+		if (csPsConfirm != csPs) {
+			$("#csPwConfirm").text("비밀번호를 다시 확인해주세요").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csPwConfirm").text("");
+		}
+	
+		// ================ 이름 ================ //
+		//1. 빈값 안됨
+		if (csNm == "") {
+			$("#csNmConfirm").text("이름을 입력해주세요").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csNmConfirm").text("");
+		}
+	
+		//2. 정규식 맞는지
+		if (!nmOnlyHangulEng.test(csNm)) {
+			$("#csNmConfirm").text("형식에 알맞은 이름을 입력해주세요.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csNmConfirm").text("");
+		}
+	
+		//3.공백이 들어갔는지
+		if (csNm.match(pattern)) {
+			$("#csNmConfirm").text("공백이 존재합니다.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csNmConfirm").text("");
+		}
+	
+		// ================ 전화번호 ================ //
+		//1.빈값 안됨
+		if (csPhoneTwo == "" || csPhoneThree == "") {
+			$("#phoneChk").text("전화번호를 입력해주세요").css("color", "red");
+			return returnValue;
+		} else {
+			$("#phoneChk").text("");
+		}
+	
+		//2.전화번호 정규식 맞는지
+		if (!phonelOnly.test(csPhoneTwo) || !phonelOnly.test(csPhoneThree)) {
+			$("#phoneChk").text("형식에 알맞은 전화번호를 입력해주세요.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#phoneChk").text("");
+		}
+	
+		//3.공백이 들어갔는지
+		if (csPhoneTwo.match(pattern) || csPhoneThree.match(pattern)) {
+			$("#phoneChk").text("공백이 존재합니다.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#phoneChk").text("");
+		}
+	
+		// ================ 생년월일 ================ //
+		//회원정보수정 만14세 미만 가입 금지
+		var date = new Date(); //Date생성자 생성
+		var thisY = date.getFullYear(); //현재년도를 YYYY로 반환한걸 thisY변수에 넣는다
+		var selectY = $("#yearBox").val(); //선택한 년도
+		if (thisY - selectY < 16) { // 올해 - 선택한년도가 16미만이면
+			$("#ageConfirm").text("*만 14세 미만 아동의 회원정보수정은 받고 있지 않습니다").css("color", "red");
+			return returnValue;
+		}
+	
+		// ================ 이메일 ================ //
+		//1. 빈값 안됨
+		if (csEmailOne == "" || csEmailTwo == "") {
+			$("#csEmailChk").text("이메일을 입력해주세요").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csEmailChk").text("");
+		}
+	
+		//2. 이메일 앞자리 정규식 맞는지
+		if (!emailOnly.test(csEmailOne)) {
+			$("#csEmailChk").text("형식에 알맞은 이메일을 입력해주세요.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csEmailChk").text("");
+		}
+	
+		//3.공백이 들어갔는지
+		if (csEmailOne.match(pattern) || csEmailTwo.match(pattern)) {
+			$("#csEmailChk").text("공백이 존재합니다.").css("color", "red");
+			return returnValue;
+		} else {
+			$("#csEmailChk").text("");
+		}
+	
+		//4.이메일 선택이면 도메인을 선택해주세요 띄우기
+		if (csEmailTwo == '선택') {
+			$("#csEmailChk").text("도메인을 선택해주세요.").css("color", "red");
+			return returnValue;
+		} else {
+			csEmailOne = "";
+		}
+	
+		//5.이미 사용중인 이메일인지
+		var resultVal="";
 		var csEmailOne = $("#csEmailOne").val(); //id가 csEmailOne인 값의 내용을 csEmailOne변수에 넣는다.
 		var csEmailTwo = $("#csEmailTwo").val(); //id가 csEmailTwo인 값의 내용을 csEmailTwo변수에 넣는다.
 		var csEmail = csEmailOne + '@' + csEmailTwo;
 		var data = {};//빈 객체 생성
 		data.csEmail = csEmail; //위에서 작성한 변수값을 'data.속성'에 넣는 작업
-		alert('2.emailChk실행');
+		//alert('2.emailChk실행');
+		
 		$.ajax({
 			url: '/myInfo/infoEmailChk',//요청 url
 			type: 'POST',//post타입
@@ -357,11 +324,15 @@ function updateInfo() {
 			success: function(result) {//성공했을시 수행하는 function
 				if (result == 0) { //DB에 저장된 이메일개수가 0개면
 					$("#csEmailChk").text("");
-					var resultVal = true;
+					resultVal = true;
 					resolve(resultVal);
-				} else {//DB에 저장된 이메일이 1개있다면(DB에 있단 소리니깐 존재하는 이메일)
+				} else if(result == 1){//본인 이메일
+					$("#csEmailChk").text("");
+					resultVal = true;
+					resolve(resultVal);
+				}else{
 					$("#csEmailChk").text("존재하는 이메일입니다. 다시 입력해주세요.").css("color", "red");
-					var resultVal = false;
+					resultVal = false;
 					resolve(resultVal);
 				}
 			},
@@ -369,78 +340,33 @@ function updateInfo() {
 				alert("관리자에게 문의하세요");
 			}
 		});
-    });*/
-
-	return true;
-	});
-}
-	
-	
-
-/*//회원정보수정 이메일 중복확인
-function emailChk(){
-	var csEmailOne = $("#csEmailOne").val(); //id가 csEmailOne인 값의 내용을 csEmailOne변수에 넣는다.
-	var csEmailTwo = $("#csEmailTwo").val(); //id가 csEmailTwo인 값의 내용을 csEmailTwo변수에 넣는다.
-	var csEmail = csEmailOne + '@' + csEmailTwo;
-	var data = {};//빈 객체 생성
-	data.csEmail = csEmail; //위에서 작성한 변수값을 'data.속성'에 넣는 작업
-
-	$.ajax({
-		url: '/myInfo/infoEmailChk',//요청 url
-		type: 'POST',//post타입
-		contentType: 'application/json',//서버에서 어떤 타입(json, html, text...)을 받을 것인지를 의미. json(key:value)형태의 데이터타입을 사용
-		data: JSON.stringify(data),//서버에서 어떤 타입(json, html, text...)을 받을 것인지를 의미. json(key:value)형태의 데이터타입을 사용
-		dataType: 'json', //요청과 함께 보낼 데이터
-		success: function(result) {//성공했을시 수행하는 function
-			if (result == 0) { //DB에 저장된 이메일개수가 0개면
-				$("#csEmailChk").text("");
-			} else {//DB에 저장된 이메일이 1개있다면(DB에 있단 소리니깐 존재하는 이메일)
-				$("#csEmailChk").text("존재하는 이메일입니다. 다시 입력해주세요.").css("color", "red");
-			}
-		},
-		error: function() {
-			alert("관리자에게 문의하세요");
-		}
-	});
-}
-*/
-
-///////////////////////////////////////////////////////////////////////////////////
-
-
-
-//두번째
-/*function aa(resultVal){
-	return new Promise(function(resolve, reject){
-		alert('3.aa실행');
-		if (updateInfo()) {
-			if (confirm('저장하시겠습니까?')) {
-				$.ajax({
-					url: '/myInfo/doUpdateInfo',
-					type: 'POST',
-					enctype: 'multipart/form-data',
-					contentType: false,//false 꼭 작성해야함
-					processData: false,//false 꼭 작성해야함,
-					data: new FormData($('#updateInfoForm')[0]),
-					success: function() {
-						alert("수정되었습니다");
-						location.href = "/myInfo/myInfoPage";
-						resolve();
-					},
-					error: function() {
-						alert("오류입니다. 관리자에게 문의해주세요");
-					}
-				});
-			}else{
-				alert("취소되었습니다");
-			}
-		}else{
-			alert("false있지롱");
-		}
 		
-    });
+		
+		/*$.ajax({
+			url: '/myInfo/infoEmailChk',//요청 url
+			type: 'POST',//post타입
+			contentType: 'application/json',//서버에서 어떤 타입(json, html, text...)을 받을 것인지를 의미. json(key:value)형태의 데이터타입을 사용
+			data: JSON.stringify(data),//서버에서 어떤 타입(json, html, text...)을 받을 것인지를 의미. json(key:value)형태의 데이터타입을 사용
+			dataType: 'json', //요청과 함께 보낼 데이터
+			success: function(result) {//성공했을시 수행하는 function
+				if (result == 0) { //DB에 저장된 이메일개수가 0개면
+					$("#csEmailChk").text("");
+					resultVal = true;
+					resolve(resultVal);
+				} else {//DB에 저장된 이메일이 1개있다면(DB에 있단 소리니깐 존재하는 이메일)
+					$("#csEmailChk").text("존재하는 이메일입니다. 다시 입력해주세요.").css("color", "red");
+					resultVal = false;
+					resolve(resultVal);
+				}
+			},
+			error: function() {
+				alert("관리자에게 문의하세요");
+			}
+		});*/
+	
+		return true;
+	});
 }
-*/
+	
 
-///////////////////////////////////////////////////////////////////////////////////
 
